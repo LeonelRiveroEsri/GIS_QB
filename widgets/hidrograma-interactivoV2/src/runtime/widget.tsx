423326106a97df2484d1667fb310bdec7b780455
Leonel Rivero <lrivero@esri.cl>
2026-07-24 12:58:34 -0400

    Actualizar Hidrograma V2 para despliegue

--- widgets/hidrograma-interactivoV2/src/runtime/widget.tsx
+++ widgets/hidrograma-interactivoV2/src/runtime/widget.tsx
@@ -189,7 +189,7 @@ const Widget = (props: AllWidgetProps<IMConfig>) => {
         <PanelConTarjetas>
           <Collapsible
             open={panelOpen}
-            title="Hidrograma-Tool"
+            title="Hidrograma-Tool (GIT)"
             onToggle={() => setPanelOpen((prev) => !prev)}
           >
             <div style={{ padding: 12 }}>
